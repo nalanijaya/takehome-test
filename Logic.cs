bb14c10c87f1b4798c654267244cfcb6fb409098
Nalani Jayasundara <nalani.jayasundara@un.org>
2022-05-05 15:59:08 +0530

    Initial commit

---
+++ Logic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shuffle
+{
+	public static class Logic
+	{
+		//This is for the following logic:
+		// Input a single integer N. Output the positive integers less than N in random order.
+		public static List<int> Shuffle(int number)
+		{
+			var numList = new List<int>();
+
+			if (number <= 0) { //Skip negative and number 0
+				return numList;
+			} else {
+				Random rd = new Random();
+				for (int i = 1; i < number; i++) { //O(n)
+					numList.Add(i);
+				}
+
+				if (numList.Count > 0) {
+					for (int i = 0; i < numList.Count; i++){  //O(n)
+						int r = rd.Next(i, numList.Count - 1); //random index
+															   //swapping
+						int temp = numList[r];
+						numList[r] = numList[i];
+						numList[i] = temp;
+					}
+				}
+			}
+			return numList;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Shuffle
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Please enter an integer");
+			var inputNumber = Console.ReadLine();
+
+			if (!string.IsNullOrEmpty(inputNumber))
+			{
+				Stopwatch sw = new Stopwatch();
+				sw.Start();
+				var shuffleNumList = Logic.Shuffle(Convert.ToInt32(inputNumber));
+				sw.Stop();
+
+				if (shuffleNumList.Count > 0) {
+					Console.WriteLine();
+					shuffleNumList.ForEach(item => Console.Write(item + " "));
+
+					Console.WriteLine();
+					Console.WriteLine();
+					Console.WriteLine("Elapsed={0}", sw.Elapsed);
+				}					
+				else {
+					Console.WriteLine("There are no positive integer less than the input integer");
+				}
+			} else {
+				Console.WriteLine("Please enter valid value. Exited !!!");
+			}
+			Console.ReadLine();
+		}
+	}
+}
